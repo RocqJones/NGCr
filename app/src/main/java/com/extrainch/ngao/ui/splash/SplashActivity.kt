ceca7b06e571e798a32d465450a98c49bbcbeb72
RocqJones <jonesmbindyo@gmail.com>
2021-06-22 11:52:05 +0300

    splash activity

---
+++ app/src/main/java/com/extrainch/ngao/ui/splash/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.extrainch.ngao.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.extrainch.ngao.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private var binding : ActivitySplashBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+
+        // hide the status bar and make the splash screen as a full screen activity
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        binding!!.getStarted.setOnClickListener(View.OnClickListener {
+            val i = Intent(this@SplashActivity, CheckUserActivity::class.java)
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(i)
+            finish()
+        })
+    }
+}
