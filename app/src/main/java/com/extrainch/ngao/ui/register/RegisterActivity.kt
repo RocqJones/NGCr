87af27671a7c9ef94438b2d38e0c2a46b0284722
RocqJones <jonesmbindyo@gmail.com>
2021-06-24 14:10:01 +0300

    register dialog

--- app/src/main/java/com/extrainch/ngao/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/extrainch/ngao/ui/register/RegisterActivity.kt
@@ -1,13 +1,25 @@
 package com.extrainch.ngao.ui.register
 
+import android.app.Dialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.extrainch.ngao.MainActivity
+import com.extrainch.ngao.R
 import com.extrainch.ngao.databinding.ActivityRegisterBinding
+import com.extrainch.ngao.databinding.DialogAlertBinding
+import com.extrainch.ngao.ui.register.viewmodel.ViewModelRegister
 import com.extrainch.ngao.ui.splash.CheckUserActivity
 
 class RegisterActivity : AppCompatActivity() {
+
     private var binding : ActivityRegisterBinding? = null
+
+    private lateinit var url : String
+    private lateinit var viewModel: ViewModelRegister
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +31,49 @@ class RegisterActivity : AppCompatActivity() {
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(i)
         }
+
+        viewModel = ViewModelProvider(this).get(ViewModelRegister::class.java)
+        viewModel.webUrl.observe(this, { url = it })
+
+        binding!!.termsAndConditionsStmt.setOnClickListener {
+            browserIntent(url)
+        }
+
+        binding!!.privacyPolicyStmt.setOnClickListener {
+            browserIntent(url)
+        }
+
+        binding!!.create.setOnClickListener {
+            if (binding!!.checkedPrivacyPolicy.isChecked && binding!!.checkedTermsAndC.isChecked) {
+                // proceed to verification
+                val j = Intent(this@RegisterActivity, MainActivity::class.java)
+                startActivity(j)
+            } else {
+                verifyCheckBoxDialog(R.style.DialogAnimation_1, "Left - Right Animation!")
+            }
+        }
+    }
+
+    private fun verifyCheckBoxDialog(animationSource: Int, s: String) {
+        val dialog = Dialog(this)
+        val dBinding: DialogAlertBinding = DialogAlertBinding.inflate(layoutInflater)
+        val v: View = dBinding.root
+        dialog.setContentView(v)
+        dialog.setCancelable(false)
+
+        dBinding.tvResponseId.text = getString(R.string.register_dialog_txt)
+
+        dBinding.dialogButtonOK.setOnClickListener{
+            dialog.dismiss()
+        }
+
+        dialog.window!!.attributes.windowAnimations = animationSource
+        dialog.show()
+    }
+
+    private fun browserIntent(url: String) {
+        val browserIntent = Intent(Intent.ACTION_VIEW)
+        browserIntent.data = Uri.parse(url)
+        startActivity(browserIntent)
     }
 }
