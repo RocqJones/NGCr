415e4bdd15bcf8a5a52ab680d8030e3b4dc66306
RocqJones <jonesmbindyo@gmail.com>
2021-06-26 17:49:26 +0300

    channels

--- app/src/main/java/com/extrainch/ngao/utils/Constants.kt
+++ app/src/main/java/com/extrainch/ngao/utils/Constants.kt
@@ -4,5 +4,10 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://197.232.33.44:56759/MbankingApi/api/"
+
+        // channels
+        const val CHANNEL_ID = "my_channel_01"
+        const val CHANNEL_NAME = "Simplified Coding Notification"
+        const val CHANNEL_DESCRIPTION = "www.simplifiedcoding.net"
     }
 }
