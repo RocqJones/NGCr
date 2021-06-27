d8aa4ca5f195d53695bde82cfa7b4f642ceeaa42
RocqJones <jonesmbindyo@gmail.com>
2021-06-27 14:30:56 +0300

    base url

--- app/src/main/java/com/extrainch/ngao/utils/Constants.kt
+++ app/src/main/java/com/extrainch/ngao/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.extrainch.ngao.utils
 class Constants {
 
     companion object {
-        const val BASE_URL = "https://197.232.33.44:56759/MbankingApi/api/"
+        const val BASE_URL = "http://192.168.1.11:8090/api/"
 
         // channels
         const val CHANNEL_ID = "my_channel_01"
